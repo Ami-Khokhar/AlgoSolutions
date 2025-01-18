148ae5a35e02f3897d1ff002c110cb53afa76598
Ami-Khokhar <136108375+Ami-Khokhar@users.noreply.github.com>
2025-01-18 11:55:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Stack using Linked List/stack-using-linked-list.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct StackNode {
+    int data;
+    StackNode *next;
+
+    StackNode(int a) {
+        data = a;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+class MyStack {
+  private:
+    StackNode *top;
+
+  public:
+    void push(int x) {
+       StackNode* temp = new StackNode(x);
+       if(!temp){
+           return;
+       }
+        if (top == NULL) { // If stack is empty, set top to temp
+            top = temp;
+        } else { // Otherwise, link the new node
+            temp->next = top;
+            top = temp;
+        }
+    }
+
+    int pop() {
+       if (top == NULL) {
+           return -1;
+        }
+        StackNode* temp = top;
+        top = top->next;
+        int popped = temp->data;
+        delete temp;
+        return popped;
+    }
+
+    MyStack() { top = NULL; }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        MyStack *sq = new MyStack();
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        int n = nums.size();
+        int i = 0;
+        while (i < n) {
+            int QueryType = nums[i++];
+            if (QueryType == 1) {
+                int a = nums[i++];
+                sq->push(a);
+            } else if (QueryType == 2) {
+                cout << sq->pop() << " ";
+            }
+        }
+        cout << endl;
+        cout << "~"
+             << "\n";
+        delete sq;
+    }
+}
+
+// } Driver Code Ends
